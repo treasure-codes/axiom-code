329b1263f92057905746b51d6cba42bb05db7cd0
treasure <akinbowaletreasure.tac@gmail.com>
2026-02-03 15:46:05 -0600

    add summary and json flags to analyze command

--- cli/analyze_cmd.py
+++ cli/analyze_cmd.py
@@ -1,9 +1,20 @@
 from axiom.orchestration.pipeline import run_analysis, run_explanation
 from axiom.output.markdown_writer import write_markdown
+from axiom.output.json_writer import write_json
+from axiom.output.summary_writer import summarize_project
 
 
 def analyze_command(args):
     project = run_analysis(args.path)
+
+    if args.summary:
+        summary = summarize_project(project)
+        if args.json:
+            write_json(summary, args.json)
+        else:
+            print(summary)
+        return
+
     explanations = run_explanation(project)
 
     if args.output:
